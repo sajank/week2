791a1a268a2ef7790d66bf95c8e005890f28b7c0
Khandelwal <sajan.khandelwal@accenture.com>
2017-10-31 12:03:57 -0500

    Sample log messages.

--- src/main/java/com/pal/tracker/WelcomeController.java
+++ src/main/java/com/pal/tracker/WelcomeController.java
@@ -1,18 +1,20 @@
 package com.pal.tracker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.json.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 @RestController
 public class WelcomeController {
+
+    static Logger log = LoggerFactory.getLogger(WelcomeController.class.getName());
 
     @PostMapping("/")
     public Response sayHello(@RequestBody MyName myname) {
@@ -28,6 +30,10 @@ public class WelcomeController {
 
     @GetMapping("/myname")
     public MyName myName() {
+        log.info("This is a info level log statement");
+        log.error("This is new error message");
+        log.debug("*************881");
+        log.debug("*************882");
         MyName myName = new MyName();
         myName.setFirstName("Mayur");
         myName.setLastName("Santani");
